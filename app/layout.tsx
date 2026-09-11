e4f6f03a20c63ebc79f0e73bfcb8d23dd8674d55
trustwalletpayments <trustwalletpayments.in@gmail.com>
2026-09-11 09:52:36 +0530

    Load ORBITEX homepage redesign styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./orbitex.css";
 import "./logo-overrides.css";
+import "./home-redesign.css";
 
 export const metadata: Metadata = {
   title: "ORBITEX Exchange",
