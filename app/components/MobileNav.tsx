5d0e8c1417c11d0478ed3debf544207e16599b1e
trustwalletpayments <trustwalletpayments.in@gmail.com>
2026-09-11 17:51:09 +0530

    Redesign mobile bottom navigation icons

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -1,12 +1,13 @@
 "use client";
 import Link from "next/link";
 import {Home,Repeat2,ChartCandlestick,WalletCards} from "lucide-react";
+import "./mobile-nav.css";
 
 export default function MobileNav(){
  return <nav className="mobile-nav">
-  <Link href="/dashboard"><Home size={22}/><span>Home</span></Link>
-  <Link href="/trade"><Repeat2 size={22}/><span>Trade</span></Link>
-  <Link href="/futures"><ChartCandlestick size={22}/><span>Futures</span></Link>
-  <Link href="/wallet"><WalletCards size={22}/><span>Assets</span></Link>
+  <Link href="/dashboard"><Home size={27} strokeWidth={2}/><span>Home</span></Link>
+  <Link href="/trade"><Repeat2 size={27} strokeWidth={2}/><span>Trade</span></Link>
+  <Link href="/futures"><ChartCandlestick size={27} strokeWidth={2}/><span>Futures</span></Link>
+  <Link href="/wallet"><WalletCards size={27} strokeWidth={2}/><span>Assets</span></Link>
  </nav>
 }
