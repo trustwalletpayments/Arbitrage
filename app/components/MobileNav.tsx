28cc5b824e24721bd0d59c21b55ea1c40ccbc273
trustwalletpayments <trustwalletpayments.in@gmail.com>
2026-09-11 17:47:40 +0530

    Simplify mobile navigation to Home Trade Futures Assets

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -1,13 +1,12 @@
 "use client";
 import Link from "next/link";
-import {Home,LineChart,Repeat2,WalletCards} from "lucide-react";
+import {Home,Repeat2,ChartCandlestick,WalletCards} from "lucide-react";
 
 export default function MobileNav(){
  return <nav className="mobile-nav">
   <Link href="/dashboard"><Home size={22}/><span>Home</span></Link>
-  <Link href="/markets"><LineChart size={22}/><span>Markets</span></Link>
   <Link href="/trade"><Repeat2 size={22}/><span>Trade</span></Link>
-  <Link href="/futures"><LineChart size={22}/><span>Futures</span></Link>
+  <Link href="/futures"><ChartCandlestick size={22}/><span>Futures</span></Link>
   <Link href="/wallet"><WalletCards size={22}/><span>Assets</span></Link>
  </nav>
 }
