43bc58745e2a806f71f5b3a19cc58cda9b5e3f1c
trustwalletpayments <trustwalletpayments.in@gmail.com>
2026-09-16 01:42:25 +0530

    Load final futures layout overrides

--- app/futures/layout.tsx
+++ app/futures/layout.tsx
@@ -1,5 +1,6 @@
 import "./mobile-overlap-fix.css";
 import "./desktop-layout-overrides.css";
+import "./final-desktop-layout.css";
 
 export default function FuturesLayout({ children }: { children: React.ReactNode }) {
   return children;
