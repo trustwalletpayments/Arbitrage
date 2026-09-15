1b6349592a8a14ac2e91dcfddabaf2dc75c3f328
trustwalletpayments <trustwalletpayments.in@gmail.com>
2026-09-16 03:47:49 +0530

    Fix Futures CSS ordering by centralizing page styles

--- app/futures/layout.tsx
+++ app/futures/layout.tsx
@@ -1,7 +1,3 @@
-import "./mobile-overlap-fix.css";
-import "./desktop-layout-overrides.css";
-import "./final-desktop-layout.css";
-
 export default function FuturesLayout({ children }: { children: React.ReactNode }) {
   return children;
 }
